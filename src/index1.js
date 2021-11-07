f1ee33e81ffd06b57ab6205efb2cbc0d8889a37a
Adam <31065348+adam8-9@users.noreply.github.com>
2021-11-07 14:41:09 +0000

    added in ship movement and restricted movement from going outside the window

--- src/index1.js
+++ src/index1.js
@@ -1,3 +1,26 @@
 import { Ship } from './ship'
 
-new Ship()
+const keys = {
+    a: false,
+    d: false,
+    ['']: false
+}
+
+document.addEventListener('keydown', (e) => {
+    keys[e.key] = true
+})
+
+document.addEventListener('keyup', (e) => {
+    keys[e.key] = false
+})
+const ship = new Ship()
+
+const update = () => {
+    if (keys['d'] && ship.x < window.innerWidth - ship.SHIP_IMAGE_WIDTH) {
+        ship.moveRight()
+    } else if (keys['a'] && ship.x > 0) {
+        ship.moveLeft()
+    }
+}
+
+setInterval(update, 20)
--- src/ship.js
+++ src/ship.js
@@ -1,9 +1,36 @@
-import shipImage from './images/ship.png'
+import shipImage from '../images/ship.png'
 
 export class Ship {
     constructor() {
-        this.el = document.createElement('img')
-        this.el.src = shipImage
-        document.body.appendChild(this.el)
+        this.el = document.createElement('img');
+        this.el.src = shipImage;
+        document.body.appendChild(this.el);
+
+        this.el.className = 'ship';
+
+        this.SPEED = 2
+        this.SHIP_IMAGE_WIDTH = 50
+
+        this.setX(window.innerWidth / 2)
+        this.setY(window.innerHeight - 80)
+
     }
+
+    setX(x) {
+        this.x = x
+        this.el.style.left = `${this.x}px`;
+    }
+
+    setY(y) {
+        this.y = y
+        this.el.style.top = `${this.y}px`;
+    }
+
+    moveRight() {
+        this.setX(this.x + this.SPEED)
+    }
+    moveLeft() {
+        this.setX(this.x - this.SPEED)
+    }
+
 }
